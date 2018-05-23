b20ea770715f2323f382d250ee8d8eb4b155327a
Yantao Du <duyantao2009@gmail.com>
2018-05-23 15:57:03 +0800

    Add python/path_util.py

---
+++ python/path_util.py
@@ -0,0 +1,14 @@
+import os
+
+# walk dir
+for root, dirs, files in os.walk('.', topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
+    for name in dirs:
+        print(os.path.join(root, name))
+
+# list file under
+print(os.listdir('.'))
+
+# get basename
+print(os.path.basename('a/bb.ccc'))
