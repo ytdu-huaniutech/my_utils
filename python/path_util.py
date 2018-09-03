5725b41b3b845476860f402cfae91e6fbd8f3681
Yantao Du <duyantao2009@gmail.com>
2018-09-03 16:42:10 +0800

    Print more in python/path_util.py; Add best_match method in python/path_util.py

--- python/path_util.py
+++ python/path_util.py
@@ -1,6 +1,8 @@
 import os
+import glob
 
 # walk dir
+print('walk dir:')
 for root, dirs, files in os.walk('.', topdown=False):
     for name in files:
         print(os.path.join(root, name))
@@ -8,12 +10,22 @@ for root, dirs, files in os.walk('.', topdown=False):
         print(os.path.join(root, name))
 
 # list file under
-print(os.listdir('.'))
+print('list dir:', os.listdir('.'))
 
 # get basename
-print(os.path.basename('a/bb.ccc'))
+print('basename of a/bb.ccc:', os.path.basename('a/bb.ccc'))
 
 # wildcard
 import glob
-for f in glob.glob("*.f"):
-    print(f)
+for f in glob.glob("*.*"):
+    print('wildcard match *.*: ', f)
+
+# test existence
+print('f exists?', os.path.exists('f'))
+
+
+# get best match files (files under dir | globbing | single file)
+def best_match_files(file_name):
+    if os.path.isdir(file_name):
+        file_name = os.path(file_name, '*')
+    return glob.glob(file_name)
