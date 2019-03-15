d0ab356a2f25e6cfa33f05319cd77f893d3192fb
Yantao Du <duyantao2009@gmail.com>
2019-03-15 16:36:52 +0800

    Add python logging util

---
+++ python/logging_util.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+
+basic_format = '%(asctime)s %(levelname)s:%(name)s: %(message)s'
+
+
+def get_logger(name, level=logging.INFO, format=basic_format):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setFormatter(logging.Formatter(format))
+    logger.addHandler(handler)
+    return logger
+
+
+if __name__ == '__main__':
+    logger = get_logger(__name__)
+    logger.info('logger created')
